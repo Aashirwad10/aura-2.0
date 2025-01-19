db6a8f332f76881cb3f749950e0576ff4afcf4fb
Aashirwad2nd <akfun6033@gmail.com>
2025-01-19 10:41:30 +0545

    new songs and album added

--- backend/src/seeds/albums.js
+++ backend/src/seeds/albums.js
@@ -48,6 +48,54 @@ const seedDatabase = async () => {
 				duration: 39, // 0:39
 			},
 			{
+				title: "खानी हो यामु",
+				artist: "Trishna Gurung",
+				imageUrl: "/cover-images/101.png",
+				audioUrl: "/songs/101.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "नाम के हो",
+				artist: "Trishna Gurung",
+				imageUrl: "/cover-images/102.png",
+				audioUrl: "/songs/102.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 60, // 0:20
+			},
+			{
+				title: "साझा को बेला",
+				artist: "Trishna Gurung",
+				imageUrl: "/cover-images/103.png",
+				audioUrl: "/songs/103.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "माया मन भरी",
+				artist: "Trishna Gurung",
+				imageUrl: "/cover-images/104.png",
+				audioUrl: "/songs/104.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "सोल्टी ज्यु",
+				artist: "Trishna Gurung",
+				imageUrl: "/cover-images/105.png",
+				audioUrl: "/songs/105.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 60, // 0:20
+			},
+			{
+				title: "गुरासै फुल्यो",
+				artist: "Trishna Gurung",
+				imageUrl: "/cover-images/106.png",
+				audioUrl: "/songs/106.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
 				title: "रीशउने भए",
 				artist: "Sushant KC",
 				imageUrl: "/cover-images/4.png",
@@ -127,6 +175,135 @@ const seedDatabase = async () => {
 				plays: Math.floor(Math.random() * 5000),
 				duration: 17, // 0:17
 			},
+			{
+				title: "चरिले त",
+				artist: "Ram Krishna Dhakal",
+				imageUrl: "/cover-images/801.png",
+				audioUrl: "/songs/801.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "बाँचुन्जेलीलाई",
+				artist: "Ram Krishna Dhakal",
+				imageUrl: "/cover-images/802.png",
+				audioUrl: "/songs/802.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "ओराली लागेको",
+				artist: "Ram Krishna Dhakal",
+				imageUrl: "/cover-images/803.png",
+				audioUrl: "/songs/803.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "पाहाड को छूक छूके रेल",
+				artist: "Ram Krishna Dhakal",
+				imageUrl: "/cover-images/804.png",
+				audioUrl: "/songs/804.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "माया रैछ र",
+				artist: "Kali Prasad Baskota",
+				imageUrl: "/cover-images/901.avif",
+				audioUrl: "/songs/901.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "लप्पन चप्पन",
+				artist: "Kali Prasad Baskota",
+				imageUrl: "/cover-images/902.avif",
+				audioUrl: "/songs/902.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "माया पिरती",
+				artist: "Kali Prasad Baskota",
+				imageUrl: "/cover-images/903.avif",
+				audioUrl: "/songs/903.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "हेरी रहन देउ मलाई",
+				artist: "Kali Prasad Baskota",
+				imageUrl: "/cover-images/904.avif",
+				audioUrl: "/songs/904.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "कया दामी भो",
+				artist: "Shiva Pariyar",
+				imageUrl: "/cover-images/1001.avif",
+				audioUrl: "/songs/1001.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "हल्ला हल्लै मई",
+				artist: "Shiva Pariyar",
+				imageUrl: "/cover-images/1002.avif",
+				audioUrl: "/songs/1002.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "पाकेर खाको फल मिठो",
+				artist: "Shiva Pariyar",
+				imageUrl: "/cover-images/1003.avif",
+				audioUrl: "/songs/1003.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "पिलायो साथी ले",
+				artist: "Shiva Pariyar",
+				imageUrl: "/cover-images/1004.avif",
+				audioUrl: "/songs/1004.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+
+			{
+				title: "बाचा भयो",
+				artist: "Swoopna Suman",
+				imageUrl: "/cover-images/501.avif",
+				audioUrl: "/songs/501.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "तिम्रो बोलीमा",
+				artist: "Swoopna Suman",
+				imageUrl: "/cover-images/502.avif",
+				audioUrl: "/songs/502.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "उपहार ",
+				artist: "Swoopna Suman",
+				imageUrl: "/cover-images/503.avif",
+				audioUrl: "/songs/503.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
+			{
+				title: "जे छौ तिमी छौ",
+				artist: "Swoopna Suman",
+				imageUrl: "/cover-images/504.avif",
+				audioUrl: "/songs/504.mp3",
+				plays: Math.floor(Math.random() * 5000),
+				duration: 20, // 0:20
+			},
 		]);
 
 		// Create albums with references to song IDs
@@ -136,29 +313,58 @@ const seedDatabase = async () => {
 				artist: "Trishna Gurung",
 				imageUrl: "/albums/1.jpg",
 				releaseYear: 2017,
-				songs: createdSongs.slice(0, 4).map((song) => song._id),
+				songs: createdSongs.slice(0, 10).map((song) => song._id),
 			},
 			{
 				title: "सारङ्गी",
 				artist: "Sushant KC",
 				imageUrl: "/albums/2.jpg",
 				releaseYear: 2023,
-				songs: createdSongs.slice(4, 8).map((song) => song._id),
+				songs: createdSongs.slice(10, 14).map((song) => song._id),
 			},
 			{
 				title: "रेशम",
 				artist: "Amrit Gurung",
 				imageUrl: "/albums/3.jpg",
 				releaseYear: 2015,
-				songs: createdSongs.slice(8, 11).map((song) => song._id),
+				songs: createdSongs.slice(14, 17).map((song) => song._id),
 			},
 			{
 				title: "सस्तो मुटु",
 				artist: "Sajjan Raj Vaidya",
 				imageUrl: "/albums/4.jpg",
 				releaseYear: 2022,
-				songs: createdSongs.slice(11, 14).map((song) => song._id),
+				songs: createdSongs.slice(17, 20).map((song) => song._id),
 			},
+			{
+				title: "फुलको आफ्नै कथा छ",
+				artist: "Ram Krishna Dhakal",
+				imageUrl: "/albums/800.jpg",
+				releaseYear: 2022,
+				songs: createdSongs.slice(20, 24).map((song) => song._id),
+			},
+			{
+				title: "हेरी रहन देउ मलाई",
+				artist: "Kali Prasad Baskota",
+				imageUrl: "/albums/600.jpg",
+				releaseYear: 2022,
+				songs: createdSongs.slice(24, 28).map((song) => song._id),
+			},
+			{
+				title: "आँशुको अर्थ",
+				artist: "Shiva Pariyar",
+				imageUrl: "/albums/700.jpg",
+				releaseYear: 2022,
+				songs: createdSongs.slice(28, 32).map((song) => song._id),
+			},
+			{
+				title: "ज छौ तिमी छौ",
+				artist: "Swoopna Suman",
+				imageUrl: "/albums/500.jpg",
+				releaseYear: 2022,
+				songs: createdSongs.slice(32, 36).map((song) => song._id),
+			},
+
 		];
 
 		// Insert all albums
